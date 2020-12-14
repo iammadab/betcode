4e82c246db9ed6fb2f4625d084f711658e7713f9
iammadab <iammadab@gmail.com>
2020-12-14 14:07:49 +0100

    Implemented the service to create a new post

--- services/post.service.js
+++ services/post.service.js
@@ -1,1 +1,22 @@
+const Post = require("../models/post")
 
+exports.createPost = async ({ tipster, odds, image, bookmakers }) => {
+
+	try{
+
+		const newPost = new Post({
+			tipster,
+			odds,
+			image,
+			bookmakers
+		})
+
+		return newPost.save()
+
+	} catch(error){
+
+		throw error
+
+	}
+
+}
