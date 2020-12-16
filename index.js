893208af40a5d057d889efc9ed678ebe38e026e0
Kinwumi <akinwumi@tutanota.com>
2020-12-16 12:14:39 +0100

    Landing page commit
    I added the frontend for the new bookmakr landing page.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 
 app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, "public")))
 
 // routes
 app.get("/", (req, res) => {
