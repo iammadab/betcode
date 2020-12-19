fd7f7684a61b1409f1b30cee64e4b4672b25c768
iammadab <iammadab@gmail.com>
2020-12-19 06:35:18 +0100

    Implemented filtering

--- public/js/tip.js
+++ public/js/tip.js
@@ -35,8 +35,6 @@ const app = new Vue({
 	
 		const tipId = new URLSearchParams(window.location.search).get("id")
 
-		console.log(tipId)
-
 		fetch(`/api/post/${tipId}`)
 			.then(res => res.json())
 			.then(data => {
