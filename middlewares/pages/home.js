fb36b08a293baee83a979da0d05c4532a848de00
iammadab <iammadab@gmail.com>
2021-03-10 19:59:51 +0000

    Implemented filtering by odds

--- middlewares/pages/home.js
+++ middlewares/pages/home.js
@@ -1,13 +1,16 @@
 const postController = require("../../controllers/post")
-const tipsterController = require("../../controllers/tipster")
+const userController = require("../../controllers/user")
 const postService = require("../../services/post.service")
 
 const home = async (req, res, next) => {
   
   const allPosts = (await postController.fetchAll({})).data
+  const allTipsters = (await userController.verifiedTipsters()).data
+  console.log(allTipsters)
 
   req.pageData = Object.assign({}, req.pageData, {
     tips: allPosts,
+    tipsters: allTipsters
   })
 
   next()
