d6587f67e4066583b3c6af53f6ba1d5b8afae2a9
iammadab <iammadab@gmail.com>
2021-03-11 11:24:22 +0000

    Added the twitter id to the user model

---
+++ apps/twitter-tipster.js
@@ -0,0 +1,13 @@
+require("dotenv").config({ path: "../.env" })
+
+const twit = require("twit")
+const T = new twit({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000
+})
+
+
+T.get("/users/lookup", { screen_name: "lifeofadunni" }).then(console.log)
