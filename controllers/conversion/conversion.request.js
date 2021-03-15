2f95eee593ec0c47958395d47612c117a88e6c70
iammadab <iammadab@gmail.com>
2021-03-15 12:38:00 +0000

    Protecting the conversion request api with token validation

--- controllers/conversion/conversion.request.js
+++ controllers/conversion/conversion.request.js
@@ -1,6 +1,24 @@
+const joi = require("joi")
+
+const requestConversionValidator = joi.object({
+  source: joi.string().lowercase().trim().required(),
+  code: joi.string().lowercase().trim().required(),
+  destination: joi.string().lowercase().trim().required(),
+  user: joi.object({
+    _id: joi.object().required(),
+    wallet: joi.number().required()
+  }).required().unknown(true)
+})
+
 const requestConversion = async (data) => {
   
+  const validationResult = requestConversionValidator.validate(data)
 
+  if(validationResult.error)
+    return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.error }
+
+  console.log("Passed validation")
+  
 }
 
 module.exports = requestConversion
