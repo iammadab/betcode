78a7faa94cbdcfd21afb8b458dba857c29ec8e93
iammadab <iammadab@gmail.com>
2021-03-15 15:18:53 +0000

    Created the conversion model, and custom function to calculate expected end time

---
+++ models/conversion.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const MINUTES = 30
+
+const conversionSchema = new mongoose.Schema({
+  source: { type: String, required: true },
+  code: { type: String, required: true },
+  destination: { type: String, required: true },
+  subscribers: { type: Array, default: [] },
+  manualSubscribers: { type: Array, default: [] },
+  tipId: { type: "ObjectId", ref: "Post" },
+  status: { 
+    type: String, 
+    default: "pending",
+    enum: [ "pending", "success", "partial", "failed" ]
+  },
+  startTime: { type: Date, default: Date.now },
+  endTime: { type: Date, default: () => { return Date.now() + MINUTES * 60000 } }
+})
+
+const conversionModel = mongoose.model("Conversion", conversionSchema)
+
+module.exports = conversionModel
