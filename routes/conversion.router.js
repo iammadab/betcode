7dc236f68f146c6b52beb322c7308b9c05009f70
iammadab <iammadab@gmail.com>
2021-03-15 12:32:53 +0000

    Created the conversion api structure

--- routes/conversion.router.js
+++ routes/conversion.router.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const conversionRouter = express.Router()
 
+const tokenMiddleware = require("../middlewares/token")
+
 const { bodyResponder } = require("../lib/adapter")
 
 const conversionController = require("../controllers/conversion")
 
 conversionRouter.post(
   "/",
+  tokenMiddleware.validateToken(),
   bodyResponder(conversionController.requestConversion)
 )
 
