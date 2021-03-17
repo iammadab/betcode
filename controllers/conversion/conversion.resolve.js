5f19d568348bdd68989d8d3316767269add2b1df
iammadab <iammadab@gmail.com>
2021-03-17 11:30:22 +0000

    Added validator to conversion resolution api

--- controllers/conversion/conversion.resolve.js
+++ controllers/conversion/conversion.resolve.js
@@ -1,7 +1,21 @@
 const conversionService = require("../../services/conversion.service")
 
-const resolveConversion = async (data) => {
+const joi = require("joi")
 
+const resolveConversionValidator = joi.object({
+  conversionId: joi.string().trim().required(),
+  status: joi.string().lowercase().trim().required(),
+  code: joi.string().lowercase().trim()
+}).options({ abortEarly: false })
+
+const resolveConversion = async (data) => {
+  
+  const validationResult = resolveConversionValidator.validate(data)
+
+  if(!validationResult.error)
+    return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.error }
+
+  data = validationResult.value
 
 }
 
