9bf27b613e53e30426ff19ff18d7283b4355f2ea
iammadab <iammadab@gmail.com>
2021-03-17 11:32:44 +0000

    Handling conversion request not found

--- controllers/conversion/conversion.resolve.js
+++ controllers/conversion/conversion.resolve.js
@@ -12,10 +12,14 @@ const resolveConversion = async (data) => {
   
   const validationResult = resolveConversionValidator.validate(data)
 
-  if(!validationResult.error)
+  if(validationResult.error)
     return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.error }
 
   data = validationResult.value
+
+  const conversionObj = await conversionService.fetchConversionById(data.conversionId)
+  if(conversionObj.error)
+    return { status: 404, code: "CONVERSION_REQUEST_NOT_FOUND" }
 
 }
 
