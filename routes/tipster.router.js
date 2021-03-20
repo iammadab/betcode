c0d412d298b9ba6826ccf4f75340bc399818c609
iammadab <iammadab@gmail.com>
2021-03-20 10:54:37 +0000

    Created an api to add new tipsters

--- routes/tipster.router.js
+++ routes/tipster.router.js
@@ -1,10 +1,37 @@
 const express = require("express")
 const tipsterRouter = express.Router()
+const makeTwitterTipster = require("../apps/twitter-tipster")
 
 const tipsterController = require("../controllers/tipster")
 const toApi = require("../lib/toApi")
 
 tipsterRouter.post("/", tipsterController.createTipster)
 tipsterRouter.get("/", toApi(tipsterController.fetchAll))
+
+tipsterRouter.post("/twitter", (req, res) => {
+
+  if(!req.body.username)
+    return res.status(400).json({
+      code: "BAD_REQUEST_ERROR",
+      message: "Twitter user name needed" 
+    })
+
+  const username = req.body.username
+
+  makeTwitterTipster(
+    username, 
+    () => {
+      res.status(200).json({
+        code: "TIPSTER_CREATED"
+      })
+    },
+    () => {
+      res.status(500).json({
+        code: "FAILED_TO_CREATE_TIPSTER"
+      })
+    }
+  )
+
+})
 
 module.exports = tipsterRouter
