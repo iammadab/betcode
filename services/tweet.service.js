3e59424033a53081a992f1d16222e481aef60ce2
iammadab <iammadab@gmail.com>
2021-03-28 12:02:11 +0000

    Showing the time for the tweet section

--- services/tweet.service.js
+++ services/tweet.service.js
@@ -1,11 +1,15 @@
 const Tweet = require("../models/tweet")
+const moment = require("moment")
 
 exports.fetchByStatus = async (status) => {
 
   try{
 
-    const tweets = await Tweet.find({ type: status })
-    return tweets
+    const now = new Date()
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate())
+
+    const tweets = await Tweet.find({ type: status, createdAt: { $gte: today }  })
+    return exports.normalizeTweets(tweets)
 
   } catch(error){
 
@@ -36,4 +40,14 @@ exports.updateTypeMany = async (ids, type) => {
 
   }
 
+}
+
+exports.normalizeTweets = tweets => {
+  return tweets.map(exports.normalizeTweet)
+}
+
+exports.normalizeTweet = tweet => {
+  const tweetObj = Object.assign({}, tweet._doc)
+  tweetObj.date = moment(tweetObj.createdAt).fromNow()
+  return tweetObj
 }
