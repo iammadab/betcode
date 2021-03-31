7e33f32bf2ebcfb753d6a414b0dffd19fa6a0542
iammadab <iammadab@gmail.com>
2021-03-31 14:08:38 +0000

    Implemented otp resend

--- public/js/pages/forgot.js
+++ public/js/pages/forgot.js
@@ -5,7 +5,9 @@ const store = {
   stage: "identity", // Can be "identity", "otp" or "password"
 
   identityInput: document.querySelector(".input-identity"),
-  numberDisplay: document.querySelector(".number-display")
+  numberDisplay: document.querySelector(".number-display"),
+  otpTimer: document.querySelector(".otp-timer"),
+  resendButton: document.querySelector(".resend-otp")
 }
 
 ;(function attachEvents(){
@@ -13,6 +15,7 @@ const store = {
   addEvent(store.inputs, "focus, change", () => {
     hideAlert(".alert-danger")
   })
+  addEvent([store.resendButton], "click", resendOtp)
 })()
 
 function selectAction(event){
@@ -45,12 +48,22 @@ function generateOtp(){
     if(response.status == 200){
       store.numberDisplay.innerText = response.data.phone 
       show(document.querySelector(".forgot-otp"))
+      startTimer()
     }
 
     else if(response.code == "USER_NOT_FOUND")
       return showAlert(".alert-danger", "Account not found")
 
   }
+
+}
+
+function resendOtp(){
+
+  const identifier = store.identityInput.value
+
+  api("/otp/forgot", { identifier })
+    .then(startTimer)
 
 }
 
@@ -64,3 +77,28 @@ function show(elem){
   hideAll()
   elem.classList.remove("hide")
 }
+
+function startTimer(){
+  showOtpTimer()
+
+  getCounter(30,
+    (seconds) => {
+      store.otpTimer.innerText = `in ${seconds} seconds`
+    },
+    () => {
+      store.otpTimer.innerText = `in 30 seconds`
+      showResendButton()
+    }
+  )
+}
+
+function showOtpTimer(){
+  store.otpTimer.classList.remove("hide")
+  store.resendButton.classList.add("hide")
+}
+
+function showResendButton(){
+  store.otpTimer.classList.add("hide")
+  store.resendButton.classList.remove("hide")
+}
+
